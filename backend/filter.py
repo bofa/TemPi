ddb07317663681f70dd43f5501b37884fa48013a
Per-Erik Karlsson <per-erik.karlsson@tieto.com>
2015-06-07 16:07:29 +0200

    Fixed the code so that it's working

--- backend/filter.py
+++ backend/filter.py
@@ -1,10 +1,13 @@
 class MyClass:
+    
+    i = 12345
+    
     """A simple example class"""
-    def __init__(self, wordList=None, adjacencyList=None):
-        i = 12345
+    def __init__(self):
+        print "init"
         
     def f(self):
         
-        i += 1
+        self.i += 1
         
-        return i
+        return self.i
